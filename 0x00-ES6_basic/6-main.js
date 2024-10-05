7d29ae3c75a0fc39f3f8acbabc9d68935d10a6d6
Toluwalase Abigail <toluabby12@gmail.com>
2024-10-05 04:28:19 +0100

    Added files for array, concat

---
+++ 0x00-ES6_basic/6-main.js
@@ -0,0 +1,6 @@
+import getSanFranciscoDescription from './6-string-interpolation.js';
+
+console.log(getSanFranciscoDescription());
+
+
+// Rewrite the return statement to use a template literal so you can the substitute the variables you’ve defined.
